6ae34a52795f2195eb70129661b8adf49b9c1bf7
건호 김 <qfourca3305@gmail.com>
2022-03-23 13:19:25 +0900

    NavBar Height

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,14 @@
+import styled from 'styled-components'
+const NavBarMainWrapper = styled.div`
+	width: 100%;
+	height: 100px;
+	border-bottom: solid 2px #eee;
+`
 const NavBar = () => {
 	return (
-		<>
+		<NavBarMainWrapper>
 			NavBar
-		</>
+		</NavBarMainWrapper>
 	)
 }
 
