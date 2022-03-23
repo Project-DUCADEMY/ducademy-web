ea648a775440872c39018a2e81f37fe8721380ca
건호 김 <qfourca3305@gmail.com>
2022-03-23 13:50:58 +0900

    NavBar Logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,30 @@
 import styled from 'styled-components'
 const NavBarMainWrapper = styled.div`
 	width: 100%;
-	height: 100px;
+	height: 80px;
 	border-bottom: solid 2px #eee;
 `
+const Logo = styled.img`
+	width: 300px;
+	position: relative;
+	float: left;
+	left: 20%;
+	top: 35px;
+`
+const MenuWrapper = styled.div`
+	background-color: black;
+	position: relative;
+	width: 40%;
+	height: 40px;
+	float: right;
+	right: 20%;
+	top: 40px;
+`
 const NavBar = () => {
-	return (
+	return (	
 		<NavBarMainWrapper>
-			NavBar
+			<Logo alt="메인 로고" src="DUCAMI.svg" />
+			<MenuWrapper/>
 		</NavBarMainWrapper>
 	)
 }
