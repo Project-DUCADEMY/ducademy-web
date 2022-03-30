547b95c3dc1843af60f9c9299b16f9a3dad5cb15
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-03-30 21:52:57 +0900

    make : carousel

--- src/components/Mainpage/Mainpage.jsx
+++ src/components/Mainpage/Mainpage.jsx
@@ -1,9 +1,7 @@
 import * as M from "./Mainpage.style"
-import {useState} from "react"
-
+import {useState,useEffect} from "react"
 
 const Mainpage = () => {
-	// const temp =["green","blue","red"]
 	const [carouselMenus,setCarouselMenus] = useState([
 		{
 			id:1,
@@ -14,37 +12,93 @@ const Mainpage = () => {
 		},{
 			id:3,
 			color:"yellowgreen"
+		},{
+			id:4,
+			color:"yellowgreen"
 		}
 	])
-	const [location,setLocation] = useState(1);
-	const goToRight = () => {
+
+	const [carouselNum,setCarouselNum] = useState(carouselMenus.length)
+	const [carouselArray,setCarouselArray] = useState([])
+
+	// 아래에 특정 인덱스로 이동할 수 있는 버튼
+	useEffect(()=>{
+		let tempArray = []
+		for (let i =0;i<carouselNum;i++){
+			tempArray.push(i)
+		}
+		setCarouselArray(tempArray)
+	},[])
+
+	const carouselScale = 1200;
+	const [translatePosition,setTranslatePosition] = useState(0);
+	function goToRight(){
+		// 내용을 넘어가지 않도록
+		if ((translatePosition-carouselScale) > -(carouselMenus.length * carouselScale)){
+			setTranslatePosition(translatePosition-carouselScale)
+		}
 
 	}
+	function goToLeft(){
+		// 바꾸기 전 비교
+		if ((translatePosition + carouselScale) <= 0){
+
+			setTranslatePosition(translatePosition + carouselScale)
+		}
+
+	}
+
+	// 캐러셀 인덱스에 따라 이동
+	function moveCarousel(index){
+		setTranslatePosition(index * (-carouselScale))
+	}
+
+	// 현재 위치에 따른 캐러셀인덱스 버튼의 색변화
 	
 	return (
-		// <M.MainContainer>
-		// 	<div>
-		// 	<M.CarouselContainer>
-		// 		{
-		// 			carouselMenus.map((i,idx)=>(
-		// 				<M.Inner backgroundColor={i.color} key={idx}>
-
-		// 				</M.Inner>
-		// 			))
-		// 		}
-		// 	</M.CarouselContainer>
-		// 	</div>
-			
-			
-		// 	{/* {
-		// 		carouselMenus.map((i,idx)=>(
-		// 		))
-		// 	} */}
-			
-		// </M.MainContainer>
+		
 		<M.MainContainer>
-			
+			<M.Carousel>
+
+			<M.CarouselWrapper>
+			<M.CarouselContainer 
+			containerWidth={1200*(carouselMenus.length)}
+			translatePosition={translatePosition}>
+				{
+					carouselMenus.map((menu) => (
+						<div key={menu.id} style={{
+							backgroundColor:menu.color
+						}}>
+							{menu.id}
+						</div>
+					))
+				}
+				
+			</M.CarouselContainer>
+			<M.CarouselLeft onClick={goToLeft}>
+				<img alt="왼쪽으로" src="Mainpage/leftArrow.svg"/>
+			</M.CarouselLeft>
+			<M.CarouselRight onClick={goToRight}>
+				<img alt="오른쪽으로" src="Mainpage/rightArrow.svg"/>
+			</M.CarouselRight>
+			</M.CarouselWrapper>
+
+			<M.CarouselChoseButtons>
+				{
+					carouselArray.map((i,idx) => (
+						<M.CarouselChoseButton 
+						key={i}
+						onClick={() => moveCarousel(i)}
+						paintCurrent={translatePosition/carouselScale == -idx ? true : false}></M.CarouselChoseButton>
+					))
+				}
+			</M.CarouselChoseButtons>
+
+			</M.Carousel>
+
 		</M.MainContainer>
+
+		
 	)
 }
 
--- src/components/Mainpage/Mainpage.style.js
+++ src/components/Mainpage/Mainpage.style.js
@@ -1,30 +1,116 @@
-import styled,{css} from 'styled-components'
+import styled, { css } from "styled-components";
 
 export const MainContainer = styled.div`
-width:100%;
-display:flex;
-flex-direction:column;
-align-items:center;
+  width: 100%;
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+`;
+
+export const Carousel = styled.div`
+    position:relative;
+    margin-top:100px;
 `
+
+export const CarouselWrapper = styled.div`
+  overflow:hidden;
+  width:1200px;
+  height:618px;
+
+  position:relative;
+
+  border-radius:20px;
+
+`
+
 export const CarouselContainer = styled.div`
-/* width:3600px; */
-width:3600px;
-/* width:fit-content; */
-height:618px;
-margin-top:60px;
-border-radius:20px;
+  
+  width: ${(props)=>(props.containerWidth+"px")};
+  height: 618px;
+  border-radius: 20px;
 
-transition:translate 0.5s;
+  /* transform:translate(-2400px); */
+  transform:translate(${(props) => (props.translatePosition+"px")});
+  transition:transform 0.7s;
+  
+  div {
+    width: 1200px;
+    height: 618px;
+    float: left;
+  }
+ 
+`;
 
-overflow-y:hidden;
+export const CarouselLeft = styled.div`
+    position:absolute;
+    width:300px;
+    height:100%;
+    top:0px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+    img{
+        margin-right:100px;
+        transition:transform 0.2s;
+    }
+    cursor:pointer;
+    :hover{
+        img{
+            transform:scale(1.5)
+        }
+    }
+
+`
+export const CarouselRight = styled.div`
+    position:absolute;
+    width:300px;
+    height:100%;
+    top:0px;
+    right:0px;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    img{
+        margin-left:100px;
+        transition:transform 0.2s;
+    }
+    cursor:pointer;
+    :hover{
+        img{
+            transform:scale(1.5);
+        }
+    }
 `
 
-export const Inner = styled.div`
-width:1200px;
-height:618px;
-background-color:${(props) => (props.backgroundColor)};
-float:left;
+export const CarouselChoseButtons = styled.div`
+    margin-right:auto;
+    margin-left:auto;
+
+    display:flex;
+    justify-content:center;
+
+    margin-top:30px;
+
+    div{
+        /* background-color:#DDDDDD; */
+
+        width:12px;
+        height:12px;
+        margin-right:14px;
+        margin-left:14px;
+        border-radius:100%;
+        cursor:pointer;
+    }
 `
 
-
-
+export const CarouselChoseButton = styled.div`
+    width:12px;
+    height:12px;
+    margin-right:14px;
+    margin-left:14px;
+    border-radius:100%;
+    cursor:pointer;
+    background-color:${(props)=>(props.paintCurrent ? "#878787" : "#DDDDDD")}
+`
