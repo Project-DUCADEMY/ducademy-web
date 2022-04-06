c4e5ffd296a054361b16d2151762bc272d9a0045
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-04-06 13:28:40 +0900

    .

--- src/style/palette.js
+++ src/style/palette.js
@@ -2,13 +2,37 @@ export const palette = {
     theme1 : {
         background: "#F33139",
         back: "#D11F2F",
-        shadow: "#FEA600",
-        text: "black"
+        shadow: "red",
+        text: "lightgray"
     },
-    theme2 :{
-        background : "#E8F5E9",
+    theme2 : {
+        background : "#3ADF00",
         back: '#2da35a',
         shadow: 'green',
         text: 'black'
+    },
+    theme3 : {
+        background : "#111111",
+        back: "gray",
+        shadow: "gray",
+        text: "white"
+    },
+    theme4 : {
+        background: '#C844E3',
+        back: '#7A3D87',
+        shadow: '#BF00FF',
+        text: 'lightgray'
+    },
+    theme5 : {
+        background: '#79D9E0',
+        back: '#7C90F6',
+        shadow: 'skyblue',
+        text: 'white'
+    },
+    theme6 : {
+        background: 'pink',
+        back : "#D78392",
+        shadow: 'pink',
+        text: 'white'
     }
 }
