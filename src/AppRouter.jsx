99940637a5a1f5abf24b56d53fc10819469455c3
Park Ji Gun <bjigeon@gmail.com>
2022-04-12 12:28:59 +0900

    Update MyPage

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -23,7 +23,8 @@ const AppRouter = () => {
 	useEffect(() => {
     	axios.post('/user/userinfo')
     	.then(response => setUserData(response.data))
-    	.catch(error => setUserData(null))
+    	// .catch(error => setUserData(null))
+    	.catch(error => setUserData(1))
 	}, [])
 
 	return (
--- src/components/MyPage/CommitGraph.jsx
+++ src/components/MyPage/CommitGraph.jsx
@@ -1,13 +1,32 @@
 import styled from 'styled-components'
 import { theme as InfoCardTheme } from './InfoCard'
-const Card = styled(InfoCardTheme)`
-	min-width: 500px;
-	max-width: 800px;
-    flex-basis: 650px;
+const Card = styled.div`
+	background-color: #fbfbfb;
+	border : 1px solid #dcdcdc;
+	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  	display: flex;
+  	flex-direction: column;
+  	display: flex;			
+  	align-content: center;
+  	align-items: center;
+  	justify-content: space-evenly;
+  	position: relative;
+    flex-shrink: 1;
+    flex-grow: 1;
+	width : 1000px;
+    flex-basis: 200px;
 `
+
+// const Card = styled(InfoCardTheme)`
+// 	min-width: 500px;
+// 	max-width: 850px;
+//     flex-basis: 650px;
+// `
+
 const render = () => {
 	return (
 		<Card></Card>
 	)
 }
-export default render
+// export default render
+export default Card
