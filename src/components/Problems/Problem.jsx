a20f7a8367aa3490d0ead06df83743c7085866d4
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-04-13 15:50:48 +0900

    Update Problem.jsx

--- src/components/Problems/Problem.jsx
+++ src/components/Problems/Problem.jsx
@@ -44,9 +44,6 @@ const Button = styled.button`
 	position: relative;
 	transition: color ease-in-out .15s;
 	cursor: pointer;
-	&:hover {
-		color: red;
-	}
 `
 const EditButton = styled(Button)`
 	&:hover { 
