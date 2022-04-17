477169cbac75c62bb31619c08754bc6ac8bb1bb7
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-04-17 22:20:34 +0900

    make [viewOfProblems]

---
+++ src/style/menu.style.js
@@ -0,0 +1,60 @@
+import styled, { css } from "styled-components";
+
+export const NewQMenus = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > div {
+    display: flex;
+  }
+`;
+
+export const NewQMenuSorted = styled.div`
+  height: 50px;
+  width: 130px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 15px 15px 0px 0px;
+
+  ${(props) =>
+    props.sortedBy
+      ? css`
+          box-shadow: 4px -4px 15px 0px #0000001a;
+        `
+      : css`
+          :hover {
+            background: #f4f4f4;
+          }
+        `}
+  cursor:pointer;
+  font-weight: bold;
+  user-select: none;
+`
+
+export const NewQMenuSubject = styled.select`
+  min-width:200px;
+  height:40px;
+  border-radius:10px;
+  border:none;
+  box-shadow: 4px 4px 15px 0px #0000001a;
+  padding-left:15px;
+  font-size:15px;
+  position: relative;
+  bottom:12px;
+  outline:none;
+  :hover{
+    background-color: #f4f4f4;
+  }
+  :focus{
+    border:solid #3fe2a6;
+  }
+  option{
+    :hover{
+      background:solid #3fe2a6;
+    }
+  }
+`
+
