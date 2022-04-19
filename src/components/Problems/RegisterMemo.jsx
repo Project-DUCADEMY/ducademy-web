92575e712ba4505ceec493e679f4f149453644c6
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-04-19 21:01:51 +0900

    Memo ProtoType

--- src/components/Problems/RegisterMemo.jsx
+++ src/components/Problems/RegisterMemo.jsx
@@ -1,25 +1,91 @@
 import styled from 'styled-components'
-const Main = styled.div`
-    width: 400px;
-    height: 400px;
-    background-color:black;
-    box-sizing: border-box;
+const Box = styled.div`
+	background-color: #fbfbfb;
+	border : 1px solid #dcdcdc;
+	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+	display: flex;
+	box-sizing: border-box;
+	justify-content: center;
+	word-break: break-all;
+	flex-wrap: wrap;
+    `
+const Main = styled(Box)`
     position: absolute;
-    width: 18vh;
-    height: 18vh;
+    width: 50%;
+    height: 60%;
     left: 50%;
     top: 50%;
     z-index: 1000;
     overflow: auto;
     outline: 0;
-    transform: rotate(0deg) scale(3)
-    translate(calc(-50% + 50px), calc(-50% + 50px));
+    transform: translate(-50%, -50%);
+    /* translate(calc(-50% + 50px), calc(-50% + 50px)); */
     display: ${(props) => (props.visable ? 'block' : 'none')};
-    z-index: 1000;
+`
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const RGBContainer = styled.div`
+    width: 100%;
+    height: 10%;
+    display: flex;
+    justify-content: space-around;
+`
+const RGBWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const RGBColor = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: ${props => props.color};
+    border-radius: 50%;
+`
+const InputTheme = styled.textarea`
+    resize: none;
+    box-sizing: border-box;
+    padding: 12px 10px;
+    width: 80%;
+    height: 80%;
+    font-size: 14px;
+    background-color: #ffffff;
+    border: 1px solid #ccc;
+    outline: none;
+    transition: box-shadow ease-in-out .15s, border-color ease-in-out .15s;
+    &:focus {
+        box-shadow: 0 0 2px #c9c9c9;
+        border-color: #bbb;
+    }
+`
+
+const BoxText = styled.h2`
+    text-align: center;
 `
 const Render = ({visable}) => {
     return(
-        <Main visable={visable}></Main>
+        <Main visable={visable}>
+            <Container>
+                <BoxText>메모 등록</BoxText>
+                <InputTheme></InputTheme>
+                <RGBContainer>
+                    <RGBWrapper>
+                        <RGBColor color={'red'}></RGBColor>
+                    </RGBWrapper>
+                    <RGBWrapper>
+                        <RGBColor color={'limegreen'}></RGBColor>
+                    </RGBWrapper>
+                    <RGBWrapper>
+                        <RGBColor color={'blue'}></RGBColor>
+                    </RGBWrapper>
+                </RGBContainer>
+            </Container>
+        </Main>
     )
 }
 export default Render;
