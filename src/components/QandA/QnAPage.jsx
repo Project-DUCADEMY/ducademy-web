899a7f0a149d5a98870135d5467f25841f018c9d
qfourca <qfourca3305@gmail.com>
2022-04-23 18:12:38 +0900

    merge fix

--- src/components/QandA/QnAPage.jsx
+++ src/components/QandA/QnAPage.jsx
@@ -1,46 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { selected } from "../../store/qna";
+import { useParams } from "react-router-dom";
 
 // import * as Q from "./QnAPage.style";
 import * as Q from "./QnAPage.style"
 
 import arrow from "../../assets/image/q&aPage/arrow.svg";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const QnAPage = () => {
-  const [question, setQuestion] = useState({
-    title: "두카미 친구들 눈을 보고 얘기합시다",
-    userId: "병관학생",
-    content:
-      "고등학교2학년 1학기 로그 문제들인데 너무 쉽네요 님들ㅇ도 풀어보셈 ㅎㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 하하하하 나는 지금 청송입니다, 가족여행을 왔죠 하ㅏ하하하하",
-    category: "기타",
-    time: "Tue Apr 12 2022 09:44:35 GMT+0900 (대한민국 표준시)",
+  const { number } = useParams()
+  const [question, setQuestion] = useState({comment: []});
+  const [comment, setComment] = useState('')
 
-  });
-
-  const [answers, setAnswers] = useState([
-    {
-      id: 0,
-      userId: "병관선생",
-      content: "이걸 못품?",
-      time: "Tue Apr 12 2022 09:44:35 GMT+0900 (대한민국 표준시)",
-    },
-    {
-      id: 1,
-      userId: "병관선생",
-      content:
-        "이걸 못품? ㄹㅇ 김건호 이어폰 낀다고 못듣는거 이해하지만 버즈 빼버리고싶다",
-      time: "Tue Apr 12 2022 09:44:35 GMT+0900 (대한민국 표준시)",
-    },
-    {
-      id: 2,
-      userId: "병관선생",
-      content:
-        "이걸 못품? ㄹㅇ 김건호 이어폰 낀다고 못듣는거 이해하지만 버즈 빼버리고싶다 이거 길이 얼마나 길어도 ㄱㄴ 한가 모르겠네 ㅋㅋㄹㄹ ",
-      time: "Tue Apr 12 2022 09:44:35 GMT+0900 (대한민국 표준시)",
-    },
-  ]);
+  useEffect(() => {
+    axios.get(`/QnA/detail?id=${number}`)
+    .then(result => {
+      console.log(result.data.QnA)
+      setQuestion(result.data.QnA)
+    })
+    .catch(console.log)
+  }, [])
 
   function makeDate(time) {
     const tempTime = new Date(time);
@@ -50,6 +32,17 @@ const QnAPage = () => {
       .toString()
       .padStart(2, "0")} / ${tempTime.getDate().toString().padStart(2, "0")}
     `;
+  }
+
+  const registerComment = () => {
+    axios.post(`/QnA/registerComment?id=${number}`, {
+      content: comment
+    })
+    .then(result => {
+      setQuestion(result.data.QnA)
+    })
+    .catch(console.log)
+    setComment('')
   }
 
   // Qestion에 있는 카테고리를 클릭했을 때 selectedSubject변경
@@ -65,7 +58,7 @@ const QnAPage = () => {
       <Q.QuestionWrapper>
         <Q.QuestionContent>
           <h2>{question.title}</h2>
-          <p>{question.content}</p>
+          <div dangerouslySetInnerHTML={{__html: question.content }}></div>
         </Q.QuestionContent>
         <Q.QuestionInfo>
           <div>
@@ -74,11 +67,11 @@ const QnAPage = () => {
             </Q.Category>
             <div>
               <i>by&#9;</i>
-              {question.userId}
+              {question.creator}
             </div>
           </div>
           <div>
-              {makeDate(question.time)}
+              {makeDate(question.day)}
           </div>
         </Q.QuestionInfo>
       </Q.QuestionWrapper>
@@ -97,7 +90,7 @@ const QnAPage = () => {
                     <i>by&#9;</i>
                     {answer.name}
                   </div>
-                  <div>{makeDate(answer.time)}</div>
+                  <div>{makeDate(answer.date)}</div>
                 </Q.AnswerInfo>
               </Q.AnswerContentWrapper>
               <Q.AnswerRecommand>
@@ -110,8 +103,8 @@ const QnAPage = () => {
           </Q.AnswerContainer>
         ))}
       </Q.AnswerWrapper>
-      {/* <Q.CommentInput value={comment} onChange={(e) => setComment(e.target.value)}/>
-      <Q.ConfirmButton onClick={registerComment}>등록</Q.ConfirmButton> */}
+      <Q.CommentInput value={comment} onChange={(e) => setComment(e.target.value)}/>
+      <Q.ConfirmButton onClick={registerComment}>등록</Q.ConfirmButton>
     </Q.Wrapper>
   );
 };
