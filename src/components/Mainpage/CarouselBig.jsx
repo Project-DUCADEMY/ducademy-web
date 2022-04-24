49b68a70b6d835c64a8bc2904752eaa3145aaf0e
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-04-24 23:55:16 +0900

    carousel content

--- src/components/Mainpage/CarouselBig.jsx
+++ src/components/Mainpage/CarouselBig.jsx
@@ -1,136 +1,193 @@
-import React,{useState,useEffect} from 'react';
-import { RecoilState } from 'recoil';
+import React, { useState, useEffect } from "react";
+import { RecoilState } from "recoil";
 
-import * as Cal from "./CarouselBig.style"
+import * as Cal from "./CarouselBig.style";
 
-import leftArrow from "../../assets/image/mainPage/carousel/leftArrow.svg"
-import rightArrow from "../../assets/image/mainPage/carousel/rightArrow.svg"
-import hoverLeftArrow from "../../assets/image/mainPage/carousel/hoverLeftArrow.svg"
-import hoverRightArrow from "../../assets/image/mainPage/carousel/hoverRightArrow.svg"
-import mouseDown from "../../assets/image/mainPage/carousel/mouseDown.svg"
-import hoverMouseDown from "../../assets/image/mainPage/carousel/hoverMouseDown.svg"
+import leftArrow from "../../assets/image/mainPage/carousel/leftArrow.svg";
+import rightArrow from "../../assets/image/mainPage/carousel/rightArrow.svg";
+import hoverLeftArrow from "../../assets/image/mainPage/carousel/hoverLeftArrow.svg";
+import hoverRightArrow from "../../assets/image/mainPage/carousel/hoverRightArrow.svg";
+import mouseDown from "../../assets/image/mainPage/carousel/mouseDown.svg";
+import hoverMouseDown from "../../assets/image/mainPage/carousel/hoverMouseDown.svg";
+
+import exam from "../../assets/image/mainPage/main/exam.svg";
+import group from "../../assets/image/mainPage/main/group.svg";
+import note from "../../assets/image/mainPage/main/note.svg";
+import question from "../../assets/image/mainPage/main/question.svg";
+import quiz from "../../assets/image/mainPage/main/quiz.svg";
+import quizzes from "../../assets/image/mainPage/main/quizzes.svg";
+
+import { Link } from "react-router-dom";
 
 const CarouselBig = () => {
+  const [carouselMenus, setCarouselMenus] = useState([
+    {
+      id: 1,
+      color: "green",
+      link: "problems",
+      comment: "원하는 분야, 수준의 문제를 풀어보세요",
+      img: quiz,
+    },
+    {
+      id: 2,
+      color: "blue",
+      link: "workbook",
+      comment: "문제집을 풀어보세요",
+      img: quizzes,
+    },
+    {
+      id: 3,
+      color: "yellowgreen",
+      link: "note",
+      comment: "문제에 대해 나만의 노트를 만들어보세요",
+      img: note,
+    },
+    {
+      id: 4,
+      color: "tomato",
+      link: "exam",
+      comment: "제한시간안에 문제를 풀어보세요",
+      img: exam,
+    },
+    {
+      id: 4,
+      color: "yellowgreen",
+      link: "QandA",
+      comment: "모르는 문제를 물어보세요",
+      img: question,
+    },
+    {
+      id: 4,
+      color: "tomato",
+      link: "group",
+      comment: "그룹을 만들어 함께 공부하세요",
+      img: group,
+    },
+  ]);
 
+  // // 카르셀 버튼 인덱스
+  // const [carouselArray,setCarouselArray] = useState([])
+  // // 아래에 특정 인덱스로 이동할 수 있는 버튼
+  // useEffect(()=>{
+  //     let tempArray = []
+  // 	for (let i =0;i<carouselMenus.length;i++){
+  //         tempArray.push(i)
+  // 	}
+  // 	setCarouselArray(tempArray)
+  // },[])
 
-    const [carouselMenus,setCarouselMenus] = useState([
-		{
-			id:1,
-			color:"green"
-		},{
-			id:2,
-			color:"blue"
-		},{
-			id:3,
-			color:"yellowgreen"
-		},{
-			id:4,
-			color:"tomato"
-		}
-	])
+  // carousel의 단위는 vw
+  const [carouselScale, setCarouselScale] = useState(100);
 
-    
-    // 카르셀 버튼 인덱스
-	const [carouselArray,setCarouselArray] = useState([])
-    // 아래에 특정 인덱스로 이동할 수 있는 버튼
-	useEffect(()=>{
-        let tempArray = []
-		for (let i =0;i<carouselMenus.length;i++){
-            tempArray.push(i)
-		}
-		setCarouselArray(tempArray)
-	},[])
+  // position의 단위는 vw
+  const [translatePosition, setTranslatePosition] = useState(0);
 
-    
-
-    // carousel의 단위는 vw
-    const [carouselScale,setCarouselScale] = useState(100)
-    
-    // position의 단위는 vw
-	const [translatePosition,setTranslatePosition] = useState(0);
-
-	function goToRight(){
-        // 내용을 넘어가지 않도록
-        if (-(carouselMenus.length * carouselScale) < translatePosition - carouselScale){
-            // translatePosition(translatePosition - carouselScale)
-            setTranslatePosition(translatePosition - carouselScale)
-        }
-	}
-	function goToLeft(){
-        // 바꾸기 전 비교
-        if(0 >= translatePosition + carouselScale){
-            // translatePosition(translatePosition + carouselScale)
-            setTranslatePosition(translatePosition + carouselScale)
-        }
-	}
-    function goToDown(){
-        window.scrollTo({
-            top:window.outerHeight,
-            behavior:"smooth"
-        })
+  function goToLeft() {
+    // 바꾸기 전 비교
+    if (0 >= translatePosition + carouselScale) {
+      // translatePosition(translatePosition + carouselScale)
+      setTranslatePosition(translatePosition + carouselScale);
+      if (translatePosition <= 0) {
+        setLeftArrowHover(false);
+      }
     }
-    
-	// 캐러셀 인덱스에 따라 이동
-	function moveCarousel(index){
-        setTranslatePosition(index * (-carouselScale))
-	}
+  }
 
-    const [leftArrowHover,setLeftArrowHover] = useState(false)
-    const [rightArrowHover,setRightArrowHover] = useState(false)
-    const [downArrowHover,setDownArrowHover] = useState(false)
+  function goToRight() {
+    // 내용을 넘어가지 않도록
+    if (
+      -(carouselMenus.length * carouselScale) <
+      translatePosition - carouselScale
+    ) {
+      // translatePosition(translatePosition - carouselScale)
+      setTranslatePosition(translatePosition - carouselScale);
+      if (translatePosition / 100 >= -(carouselMenus.length - 1)) {
+        setRightArrowHover(false);
+      }
+    }
+  }
+  function goToDown() {
+    window.scrollTo({
+      top: window.outerHeight,
+      behavior: "smooth",
+    });
+  }
 
-    return (
-        <Cal.Carousel>
-            <Cal.CarouselWrapper>
-                <Cal.CarouselContainer
-                    containerWidth={100*(carouselMenus.length)}
-                    translatePosition={translatePosition}
-                >
-                    {
-                        carouselMenus.map((menu) => (
-                            <Cal.carouselMenu
-                            key={menu.id}
-                            style={{backgroundColor:menu.color}}
-                            carouselScale={carouselScale}
-                            ></Cal.carouselMenu>
-                        ))
-                    }
-                </Cal.CarouselContainer>
-                <Cal.CarouselMove 
-                onClick={goToLeft}
-                Right={false}
-                onMouseEnter={() => setLeftArrowHover(true)}
-                onMouseLeave={() => setLeftArrowHover(false)}
-                >
-                    <img 
-                    alt="왼쪽으로" 
-                    src={leftArrowHover ? hoverLeftArrow:leftArrow}
-                    />
-                </Cal.CarouselMove>
-                <Cal.CarouselMove 
-                onClick={goToRight}
-                Right={0}
-                onMouseEnter={() => setRightArrowHover(true)}
-                onMouseLeave={() => setRightArrowHover(false)}
-                >
-                    <img 
-                    alt="오른쪽으로" 
-                    src={rightArrowHover ? hoverRightArrow : rightArrow} 
-                    />
-                </Cal.CarouselMove>
-                <Cal.CarouselDown
-                onClick={goToDown}
-                onMouseEnter={() => setDownArrowHover(true)}
-                onMouseLeave={() => setDownArrowHover(false)}
-                >
-                    <img 
-                    alt="아래쪽으로"
-                    src={downArrowHover ? hoverMouseDown : mouseDown}
-                    /> 
-                </Cal.CarouselDown>
-            </Cal.CarouselWrapper>
-        </Cal.Carousel>
-    );
+  // 캐러셀 인덱스에 따라 이동
+  function moveCarousel(index) {
+    setTranslatePosition(index * -carouselScale);
+  }
+
+  const [leftArrowHover, setLeftArrowHover] = useState(false);
+  const [rightArrowHover, setRightArrowHover] = useState(false);
+  const [downArrowHover, setDownArrowHover] = useState(false);
+
+  return (
+    <Cal.Carousel>
+      <Cal.CarouselWrapper>
+        <Cal.CarouselContainer
+          containerWidth={100 * carouselMenus.length}
+          translatePosition={translatePosition}
+        >
+          {carouselMenus.map((menu) => (
+            <Cal.carouselMenu
+              key={menu.id}
+              //   style={{ backgroundColor: menu.color }}
+              carouselScale={carouselScale}
+            >
+              {/* <h2>{menu.comment}</h2>
+              <img src={menu.img} /> */}
+              {/* <Link to={menu.link}>
+                <Cal.CarouselMenuContent> */}
+              <div>
+                <h2>{menu.comment}</h2>
+                <img src={menu.img} />
+              </div>
+              {/* </Cal.CarouselMenuContent>
+              </Link> */}
+            </Cal.carouselMenu>
+          ))}
+        </Cal.CarouselContainer>
+        {translatePosition < 0 && (
+          <Cal.CarouselMove
+            onClick={goToLeft}
+            Right={false}
+            onMouseEnter={() => setLeftArrowHover(true)}
+            onMouseLeave={() => setLeftArrowHover(false)}
+          >
+            <img
+              alt="왼쪽으로"
+              src={leftArrowHover ? hoverLeftArrow : leftArrow}
+            />
+          </Cal.CarouselMove>
+        )}
+
+        {translatePosition / 100 > -(carouselMenus.length - 1) && (
+          <Cal.CarouselMove
+            onClick={goToRight}
+            Right={0}
+            onMouseEnter={() => setRightArrowHover(true)}
+            onMouseLeave={() => setRightArrowHover(false)}
+          >
+            <img
+              alt="오른쪽으로"
+              src={rightArrowHover ? hoverRightArrow : rightArrow}
+            />
+          </Cal.CarouselMove>
+        )}
+        <Cal.CarouselDown
+          onClick={goToDown}
+          onMouseEnter={() => setDownArrowHover(true)}
+          onMouseLeave={() => setDownArrowHover(false)}
+        >
+          <img
+            alt="아래쪽으로"
+            src={downArrowHover ? hoverMouseDown : mouseDown}
+          />
+        </Cal.CarouselDown>
+      </Cal.CarouselWrapper>
+    </Cal.Carousel>
+  );
 };
 
 export default CarouselBig;
--- src/components/Mainpage/CarouselBig.style.js
+++ src/components/Mainpage/CarouselBig.style.js
@@ -1,64 +1,94 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Carousel = styled.div`
-    position:relative;
-`
+  position: relative;
+`;
 
 export const CarouselWrapper = styled.div`
-    overflow:hidden;
-    width:100vw;
-    height:97vh;
-    position:relative;
-`
+  overflow: hidden;
+  width: 100vw;
+  height: 97vh;
+  position: relative;
+`;
 
 export const CarouselContainer = styled.div`
-    width:${(props)=>(props.containerWidth+"vw")};
-    transform:translate(${(props) => (props.translatePosition+"vw")});
-    height:100vh;
-    transition:transform 0.7s;
-`
+  width: ${(props) => props.containerWidth + "vw"};
+  transform: translate(${(props) => props.translatePosition + "vw"});
+  height: 100vh;
+  transition: transform 0.7s;
+`;
 
 export const carouselMenu = styled.div`
-    width:${(props) => (props.carouselScale+"vw")};
-    height:100vh;
-    float:left;
-    filter:brightness(70%);
+  width: ${(props) => props.carouselScale + "vw"};
+  height: 100vh;
+  float: left;
+  /* filter:brightness(70%); */
 
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    /* background-color:blue; */
+    width: 60%;
+    height: 60%;
+    margin-bottom: 100px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius:15px;
+    box-shadow: 4px 4px 15px 0px #0000001a;
+    cursor:pointer;
+    img {
+      /* width: 60%; */
+      height: 60%;
+      user-select: none;
+      margin-left:50px;
+    }
+    h2{
+        margin-right:50px;
+    }
+    :hover{
+        color:white;
+        background-color:#3fe2a6;
+    }
+  }
+`;
+
+
+export const CarouselMenuContent = styled.div``
+
 
 export const CarouselMove = styled.div`
-    position:absolute;
-    width: 300px;
-    height: 50%;
-    /* top:0px; */
-    top:25%;
-    right:${(props)=>(props.Right)};
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    cursor:pointer;
-    img{
-        transition:transform 0.2s;
-        user-select:none;
-        fill:white;
-        
-    }
-    /* :hover{
-        img{
-            transform:scale(1.3);
-        }
-    } */
-`
+  position: absolute;
+  width: 300px;
+  height: 50%;
+  /* top:0px; */
+  top: 25%;
+  right: ${(props) => props.Right};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  img {
+    transition: transform 0.2s;
+    user-select: none;
+    fill: white;
+    user-select: none;
+  }
+`;
 
 export const CarouselDown = styled.div`
-    position:absolute;
-    width:200px;
-    height:200px;
-    bottom:0px;
-    left:45vw;
-    display:flex;
-    flex-direction:column;
-    justify-content:space-around;
-    align-items:center;
-    cursor:pointer;
-`
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  bottom: 0px;
+  left: 45vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
+`;
