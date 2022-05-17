d9af4fc6ff68ecbd2d9249ccc9aefdd019bc0c6c
bjigeon <bjigeon@gmail.com>
2022-05-17 17:57:30 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import styled from 'styled-components'
-import AppRouter from './AppRouter';
+import styled from "styled-components";
+import AppRouter from "./AppRouter";
 const AppStyle = styled.div`
-	margin-top: 75px;
-	min-height: calc(100vh - 75px);
-`
+  margin-top: 75px;
+  min-height: calc(100vh - 75px);
+`;
 const Background = styled.div`
-	width: 100%;
-	min-height: calc(100vh - 75px);
-	margin-top: 75px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	background-color: white;
-`
+  width: 100%;
+  min-height: calc(100vh - 75px);
+  margin-top: 75px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+`;
 const App = () => {
-	return (
-		<Background>
-			<AppRouter/>
-		</Background>
-	)
-}
-export default App
+  return (
+    <Background>
+      <AppRouter />
+    </Background>
+  );
+};
+export default App;
